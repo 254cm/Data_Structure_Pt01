7c9abf2f050ab65bbbc0231d53cca391590d3ce7
오재민 <the254jm@gmail.com>
2019-03-06 18:27:03 +0900

    1차 수정

--- Practice_01.java
+++ Practice_01.java
@@ -2,35 +2,60 @@
 public class Practice_01 {
 
 	public static void main(String[] args) {
-		Wordlist Head = new Wordlist();
-		init_class(Head);
-		for(Wordlist i = Head;i == null ;i = i.Alpha)
+		
+		wordlist[] book;
+		book = new wordlist[26];
+		for(char i ='a';i<='z';i++)		// wordlist의 alpha 초기화
 		{
-			System.out.println(i.word);
+			book[(int)(i-'a')].Alpha = i; 
 		}
-
-	}
-	
-	static void init_class(Wordlist node)
-	{
-		for(String i = "a";i.equals("z");i+=1)
+		String input = "abdc";
+		while(true)
 		{
-			node.word = i;
-			node.Alpha = new Wordlist();
-			node = node.Alpha;
+			if(input.length() == 1)
+			{
+				for(int i = 0;i<=25;i++)
+				{
+					if(input.equals(Character.toString(book[i].Alpha)))
+					{
+						book[i].count++;
+						break;
+					}
+				}
+			}
+			else
+			{
+				for(int i=0;i<=25;i++)
+				{
+					if(input.charAt(0) == book[i].Alpha)
+					{
+						
+					}
+				}
+			}
 		}
 	}
-
 }
 
-class Wordlist
+class wordlist
 {
-	String word;
-	Wordlist next;
-	Wordlist Alpha;
+	char Alpha;
+	int count = 0;
+	words head = null;
 	
 	void add (String word)
 	{
 		
+	}
+}
+
+class words // 상위클래스에서 분류된 이니셜알파벳을 가진 단어들의 리스트
+{
+	String word;
+	int count = 0;
+	words next = null;
+	public words (String word)
+	{
+		this.word = word;
 	}
 }
