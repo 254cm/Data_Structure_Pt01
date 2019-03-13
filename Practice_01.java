0cf0112960417d1189accb14b6223e29758aeb87
오재민 <the254jm@gmail.com>
2019-03-13 09:21:55 +0900

    Master) 개발 1주일차

--- Practice_01.java
+++ Practice_01.java
@@ -10,9 +10,9 @@ public class Practice_01 {
 			book[(int)(i-'a')].Alpha = i; 
 		}
 		String input = "abdc";
-		while(true)
+		while(true)						// 입력문자의 분류
 		{
-			if(input.length() == 1)
+			if(input.length() == 1)		// 1차 분류 -> 입력된 단어가 단 1개짜리 알파벳일때
 			{
 				for(int i = 0;i<=25;i++)
 				{
@@ -23,12 +23,25 @@ public class Practice_01 {
 					}
 				}
 			}
-			else
+			else						// 2차 분류 -> 입력된 단어가 1개짜리가 아닌 경우
 			{
-				for(int i=0;i<=25;i++)
+				for(int i=0;i<=25;i++)	// 입력된 단어의 첫 글자와 일치하는 알파벳 찾기 반복
 				{
-					if(input.charAt(0) == book[i].Alpha)
+					if(input.charAt(0) == book[i].Alpha)	// 입력된 단어의 첫 글자와 알파벳이 일치 할 경우
 					{
+						words test = book[i].head;			// 새로운 분기 발생: book[i]의 head가 null일때 or아닐때
+						
+						if(test == null)					// head의 참조 test가 null일때는 head 자체가 널이거나 리스트의 끝단 일 경우 
+						{
+							test = new words(input);		
+							test.next = null;
+							test = test.next;
+						}
+						
+						else
+						{
+							
+						}
 						
 					}
 				}
